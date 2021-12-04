d11b2f2e899c0da9b5172bf11aba024ce0b96a15
Dirk Zukunft <87307560+dirkzukunft@users.noreply.github.com>
2021-12-04 06:48:38 +0100

    Add basic directives and event handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-playground';
+  list: string[] = [];
+  newItem: string = '';
+
+  addItem() {
+    this.list.push(this.newItem);
+    this.newItem = '';
+  }
+
+  updateNewItem(newValue: string) {
+    this.newItem = newValue;
+  }
 }
